a46b20b09e856d4976e58e889bfbb6bd1d3bd517
Erwin van der Valk <erwin.vandervalk@evision-software.com>
2014-10-07 15:38:55 +0200

    Temporary added autofacnsubstitutemockingcontainer

--- Source/Chill.Examples.Tests/CustomerController.cs
+++ Source/Chill.Examples.Tests/CustomerController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Autofac.Core;
+using AutofacContrib.NSubstitute;
 using Chill.Examples.Tests.TestSubjects;
 
 namespace Chill.Examples.Tests
@@ -35,5 +36,25 @@ namespace Chill.Examples.Tests
         }
 
 
+    }
+
+    public class AutofacNSubstituteAutoMockingContainer : IAutoMockingContainer
+    {
+        private AutoSubstitute container = new AutoSubstitute();
+        public T Get<T>() where T : class
+        {
+            return container.Resolve<T>();
+        }
+
+        public T Set<T>(T valueToSet) where T : class
+        {
+            return container.Provide(valueToSet);
+
+        }
+
+        public void Dispose()
+        {
+            container.Dispose();
+        }
     }
 }
