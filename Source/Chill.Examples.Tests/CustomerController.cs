59b48ce101d7212b8fcf96eadb966927fff5f88a
Erwin van der Valk <erwin.vandervalk@evision-software.com>
2014-10-07 16:36:03 +0200

    Updated extensions to install code

--- Source/Chill.Examples.Tests/CustomerController.cs
+++ Source/Chill.Examples.Tests/CustomerController.cs
@@ -38,23 +38,4 @@ namespace Chill.Examples.Tests
 
     }
 
-    public class AutofacNSubstituteAutoMockingContainer : IAutoMockingContainer
-    {
-        private AutoSubstitute container = new AutoSubstitute();
-        public T Get<T>() where T : class
-        {
-            return container.Resolve<T>();
-        }
-
-        public T Set<T>(T valueToSet) where T : class
-        {
-            return container.Provide(valueToSet);
-
-        }
-
-        public void Dispose()
-        {
-            container.Dispose();
-        }
-    }
 }
