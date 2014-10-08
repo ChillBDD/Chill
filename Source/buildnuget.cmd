705198d4e05b04e2f944a9b537a091e3f47f2440
Erwin van der Valk <erwin.vandervalk@evision-software.com>
2014-10-08 15:47:15 +0200

    fixed buildnuget.cmd to pack the chill project

--- Source/buildnuget.cmd
+++ Source/buildnuget.cmd
@@ -1,3 +1,3 @@
-nuget pack Source\Chill\.nuspec -BasePath Source\Chill
+nuget pack Source\Chill\chill.csproj -BasePath Source\Chill
 nuget pack Source\Chill.AutoFacNSubstitute\.nuspec -BasePath Source\Chill.AutoFacNSubstitute
 nuget pack Source\Chill.AutoFacFakeItEasy\.nuspec -BasePath Source\Chill.AutoFacFakeItEasy
