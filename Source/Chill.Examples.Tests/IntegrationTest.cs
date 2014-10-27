c5241039de9bfe87dd93f69ca84e386b6d1da6b0
Erwin van der Valk <erwin.vandervalk@evision-software.com>
2014-10-27 20:30:41 +0100

    Moved autofac container tests to chill test project

--- Source/Chill.Examples.Tests/IntegrationTest.cs
+++ Source/Chill.Examples.Tests/IntegrationTest.cs
@@ -13,67 +13,6 @@ namespace Chill.Examples.Tests
 {
 
 
-    public class AutofacChillContainerSpecs
-    {
-        
-        [ChillTestInitializer(typeof(DefaultChillTestInitializer<AutofacContainerWithCustomModule>))]
-        public class When_configuring_container_with_module_and_objectmother : GivenWhenThen
-        {
-            [Fact]
-            public void Then_testservice_is_registered_through_module()
-            {
-                The<ITestService>().GetType().Should().Be(typeof(TestService));
-            }
-
-            [Fact]
-            public void Then_customer_is_resolved_through_Customer_Automother()
-            {
-                The<Customer>().Name.Should().NotBeNull();
-            }
-
-            [Fact]
-            public void Then_cannot_resolve_unknown_interface()
-            {
-                Action a = () => The<IAppDomainSetup>();
-
-                a.ShouldThrow<ComponentNotRegisteredException>();
-            }
-
-
-        }
-
-        /// <summary>
-        /// You can define 'non mocking' with all the type registrations that you would otherwise in your application. 
-        /// This can either be done in the App
-        /// </summary>
-        internal class AutofacContainerWithCustomModule : AutofacChillContainer
-        {
-            public AutofacContainerWithCustomModule()
-                : base(CreateContainerBuilder())
-            {
-
-            }
-
-            private static ContainerBuilder CreateContainerBuilder()
-            {
-                var builder = new ContainerBuilder();
-                builder.RegisterModule<CustomAutofacModule>();
-                return builder;
-            }
-        }
-
-
-
-        public class CustomAutofacModule  : Module
-        {
-            protected override void Load(ContainerBuilder builder)
-            {
-                builder.RegisterType<TestService>().As<ITestService>();
-                base.Load(builder);
-            }
-        }
-
-    }
 
     public class IntegrationTests
     {
@@ -92,15 +31,6 @@ namespace Chill.Examples.Tests
 
 
     
-    public interface ITestService
-    {
-
-    }
-    public class TestService : ITestService
-    {
-
-    }
-
     public class ServerMother : ObjectMother<Server>
     {
         protected override Server Create()
---
+++ Source/Chill.Tests.Shared/CoreScenarios/AutofacChillContainerSpecs.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using Chill.Tests.TestSubjects;
+using Chill.Autofac;
+using Autofac.Core.Registration;
+using Autofac;
+
+namespace Chill.Tests.CoreScenarios
+{
+    public class AutofacChillContainerSpecs
+    {
+        /// <summary>
+        /// This test checks if you can use Chill as an integration test. It sets up a regular autofac
+        /// container (without mocking) using an autofac module <see cref="AutofacContainerWithCustomModule"/>. 
+        /// </summary>
+        [ChillTestInitializer(typeof(DefaultChillTestInitializer<AutofacContainerWithCustomModule>))]
+        public class When_configuring_container_with_module_and_objectmother : GivenWhenThen
+        {
+            /// <summary>
+            /// Validate that the <see cref="AutofacContainerWithCustomModule" /> has registered the ITestService properly.
+            /// </summary>
+            [Fact]
+            public void Then_testservice_is_registered_through_module()
+            {
+                The<ITestService>().GetType().Should().Be(typeof(TestService));
+            }
+
+            /// <summary>
+            /// The Subject_built_By_Chill_AutoMother should be constructed by an automother. 
+            /// </summary>
+            [Fact]
+            public void Then_customer_is_resolved_through_Customer_Automother()
+            {
+                The<Subject_built_By_Chill_AutoMother>().Name.Should().NotBeNull();
+            }
+
+            /// <summary>
+            /// Any types not explicitly registered in the container should not be resolvable. 
+            /// </summary>
+            [Fact]
+            public void Then_cannot_resolve_unknown_interface()
+            {
+                Action a = () => The<IAppDomainSetup>();
+
+                a.ShouldThrow<ComponentNotRegisteredException>();
+            }
+        }
+
+
+        /// <summary>
+        /// You can define 'non mocking' with all the type registrations that you would otherwise in your application. 
+        /// This can either be done in the App
+        /// </summary>
+        internal class AutofacContainerWithCustomModule : AutofacChillContainer
+        {
+            public AutofacContainerWithCustomModule()
+                : base(CreateContainerBuilder())
+            {
+
+            }
+            /// <summary>
+            /// This method creates the Autofac container and registers the custom type 
+            /// </summary>
+            /// <returns></returns>
+            private static ContainerBuilder CreateContainerBuilder()
+            {
+                var builder = new ContainerBuilder();
+                builder.RegisterModule<CustomAutofacModule>();
+                return builder;
+            }
+        }
+
+
+
+        public class CustomAutofacModule : Module
+        {
+            protected override void Load(ContainerBuilder builder)
+            {
+                builder.RegisterType<TestService>().As<ITestService>();
+                base.Load(builder);
+            }
+        }
+
+        public interface ITestService
+        {
+
+        }
+        public class TestService : ITestService
+        {
+
+        }
+
+    }
+
+}
