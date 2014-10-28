bb6b8e70b23c308ba970c92772b7effb1d0b7717
Erwin van der Valk <erwin.vandervalk@evision-software.com>
2014-10-28 14:56:35 +0100

    Cleaned up tests

--- Source/Chill.Tests.Shared/CoreScenarios/TestBaseSpecs.cs
+++ Source/Chill.Tests.Shared/CoreScenarios/TestBaseSpecs.cs
@@ -1,8 +1,10 @@
 using Chill.Tests.TestSubjects;
-
 using FluentAssertions;
-
+using System;
+using System.Threading;
 using Xunit;
+using Xunit.Extensions;
+using TestClass = Chill.Tests.TestSubjects.TestClass;
 
 namespace Chill.Tests.CoreScenarios
 {
@@ -39,6 +41,47 @@ namespace Chill.Tests.CoreScenarios
         {
             SetThe<TestClass>().To(new TestClass("abc"));
             The<TestClass>().Name.Should().Be("abc");
+        }
+
+
+        /// <summary>
+        /// Testing if Dispose() is called on our subjects is tricky, because this happens AFTER a test, it's hard to test if it is actually called
+        /// Here's a trick i'm doing. I'm incrementing a value in a test and decrementing it in the constructor. Then i'm using locking to ensure these tests
+        /// only execute one at a time. 
+        /// 
+        /// If the dispose method was not called, then 
+        ///     1. the tests would likely not complete because the monitor.exit is never called. 
+        ///     2. the Alive property will sometimes be true. 
+        /// 
+        /// The Theory and the InlineData attribute causes the test to be called several times. 
+        /// </summary>
+        #region Tests_if_disposable_is_called
+        [Theory, InlineData(), InlineData(), InlineData()]
+    
+        public void Will_dispose_object()
+        {
+            DisposableObject.DisposableObjectAlive.Should().BeFalse(); 
+            var target = new DisposableObject();
+            UseThe(target);
+        }
+
+        #endregion
+    }
+
+    public class DisposableObject : IDisposable
+    {
+        static object syncroot = new object();
+        public static bool DisposableObjectAlive = false;
+
+        public DisposableObject()
+        {
+            Monitor.Enter(syncroot);
+            DisposableObjectAlive = true;
+        }
+        public void Dispose()
+        {
+            DisposableObjectAlive = false;
+            Monitor.Exit(syncroot);
         }
     }
 }
