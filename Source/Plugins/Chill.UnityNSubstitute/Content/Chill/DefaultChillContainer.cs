6780d2cae7d8bf0906c1846f6b888be9420b7f2e
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2015-01-18 13:00:36 +0100

    When running in a Silverlight test host, Chill is giving internal visibility in the unit test assembly. Should fix #26.

--- Source/Plugins/Chill.UnityNSubstitute/Content/Chill/DefaultChillContainer.cs
+++ Source/Plugins/Chill.UnityNSubstitute/Content/Chill/DefaultChillContainer.cs
@@ -4,3 +4,7 @@ using Chill.UnityNSubstitute;
 // This attribute defines which container will be used by default for this assembly
 
 [assembly: ChillContainer(typeof(UnityNSubstituteChillContainer))]
+
+#if SILVERLIGHT
+[assembly: InternalsVisibleTo("Chill")]
+#endif
