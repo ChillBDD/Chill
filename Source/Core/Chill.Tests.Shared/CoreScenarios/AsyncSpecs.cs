5699e5e478af8188320a445ea3ad81eeb552cad2
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2016-02-28 14:14:55 +0100

    SAVEPOINT

---
+++ Source/Core/Chill.Tests.Shared/CoreScenarios/AsyncSpecs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Chill.Tests.CoreScenarios
+{
+    namespace AsyncSpecs
+    {
+        public class When_an_async_act_is_used : GivenWhenThen
+        {
+            private string result;
+
+            public When_an_async_act_is_used()
+            {
+                When(async () =>
+                {
+                    await Task.Delay(1000.Milliseconds());
+                    result = await Task.FromResult("hello from async WHEN");
+                });
+            }
+
+            [Fact]
+            public void Then_it_should_evaluate_the_sync_code_synchronously()
+            {
+                result.Should().Contain("hello");
+            }
+        }
+    }
+}
